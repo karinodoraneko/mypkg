9c2b3da53826738dd5a0089c2271320401230dae
Yuto Matsushima <karinodoraneko@gmail.com>
2025-12-30 09:18:23 +0900

    Fix test

--- test/test.bash
+++ test/test.bash
@@ -7,8 +7,9 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source install/setup.bash
+source $dir/.bashrc
 
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log | grep 'Battery'
+cat /tmp/mypkg.log |
+grep 'Battery'
